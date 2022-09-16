9dc6c9e638542e8388d75675dc868595582ca06f
Prajwal Rao <prajwalraob@yahoo.in>
2022-09-16 17:04:23 +0530

    init

---
+++ tryapp/models.py
@@ -0,0 +1,8 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+	title = models.TextField()
+	description = models.TextField()
+	price = models.TextField()
